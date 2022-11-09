e151fe19fd8e0d9a4a83ad578beef9a04686259e
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-11-09 22:46:08 +0600

    API created for sending reviews data from client side

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -11,22 +11,40 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ertblk.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const serviceCollection = client.db('photoMagic').collection('services');
+        const reviewCollection = client.db('photoMagic').collection('reviews');
 
         //get all services data from database
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
-            const cursor2 = serviceCollection.find(query).limit(3)
+            const cursor2 = serviceCollection.find(query).limit(3);
             const limitServices = await cursor2.toArray();
             const services = await cursor.toArray();
             res.send({ limitServices, services })
         });
+
+        //get particuller service data from database using id
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //post reviews using client data (api)
+        app.post('/reviews', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const result = await reviewCollection.insertOne(order);
+            res.send(result)
+        });
+
     }
     finally {
 
