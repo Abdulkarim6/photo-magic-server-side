146bb6da0b9091d34bf61d169157ae937c2a52c0
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-11-10 20:47:20 +0600

    create api for delete review

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ async function run() {
             res.send(reviews)
         });
 
+        //delete data from database for delete review
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
